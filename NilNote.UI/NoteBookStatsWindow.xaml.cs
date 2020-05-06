6ce53ffc9da3db1926554cd2181fc10b5ea0c14d
Patrick Rećko <guest_none@protonmail.com>
2020-05-06 23:41:30 +0200

    WIP: GUI Design - Initial design for Main Window - Skeleton for other windows

---
+++ NilNote.UI/NoteBookStatsWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NilNote.UI
+{
+    /// <summary>
+    /// Interaction logic for NoteBookStatsWindow.xaml
+    /// </summary>
+    public partial class NoteBookStatsWindow : Window
+    {
+        public NoteBookStatsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
