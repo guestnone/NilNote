d741f342a359b962e0f533440af5ffe67cf9656e
Patrick Rećko <guest_none@protonmail.com>
2020-05-14 14:03:01 +0200

    WIP

---
+++ NilNote.Core/FileUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NilNote.Core
+{
+    class FileUtils
+    {
+        public static string GetSaveLocationInAppDataFolder()
+        {
+            var userPath = Environment.GetEnvironmentVariable(
+              RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
+              "LOCALAPPDATA" : "Home");
+
+            var assy = System.Reflection.Assembly.GetEntryAssembly();
+            var companyName = assy.GetCustomAttributes<AssemblyCompanyAttribute>()
+              .FirstOrDefault();
+            var path = System.IO.Path.Combine(userPath, companyName.Company, "NilNote");
+
+            return path;
+        }
+
+    }
+}
---
+++ NilNote.Core/NoteBookManager.cs
@@ -0,0 +1,52 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NilNote.Core
+{
+    public sealed class NoteBookManager
+    {
+        private static NoteBookManager mInstance = null;
+
+        private string mConfigPath;
+        private string mDbPath;
+        private bool mHasExistingDb = false;
+        private LiteDatabase mDatabase;
+
+        public bool HasExistingDB { get => mHasExistingDb; }
+
+        public static NoteBookManager Instance
+        {
+            get
+            {
+                if (mInstance == null)
+                {
+                    mInstance = new NoteBookManager();
+                }
+                return mInstance;
+            }
+        }
+
+        private NoteBookManager()
+        {
+            mConfigPath = FileUtils.GetSaveLocationInAppDataFolder();
+            mDbPath = mConfigPath + "/nb_" + Environment.UserName + ".db";
+            if (File.Exists(mDbPath))
+            {
+                mDatabase = new LiteDatabase(mDbPath);
+                mHasExistingDb = true;
+            }
+            mHasExistingDb = false;
+        }
+
+        private void SetNewUserDatabase(NoteBookDetails details)
+        {
+            mDatabase = new LiteDatabase(mDbPath);
+
+        }
+        
+
+    }
+}
